aacfd510da6b527560663189a6200f8f27a8bf7c
Ela_M <ela.majcher@gmail.com>
2019-02-24 11:09:52 +0100

    with Mismatch exception

--- src/main/java/currencySimpleCalculator/CurrencyCalculator.java
+++ src/main/java/currencySimpleCalculator/CurrencyCalculator.java
@@ -3,6 +3,7 @@ package currencySimpleCalculator;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -20,22 +21,32 @@ public class CurrencyCalculator {
         AverageRate.showActualAverageValuesUsaEurGbpChf();
         System.out.println();
 
+
         do {
+            try {
 
-            System.out.println("***********************************************************");
-            System.out.println("To check how much can you buy today fot 100 PLN - enter 1");
-            System.out.println("To compare today with last month currency value - enter 2");
-            System.out.println("Enter - 3 - to end session");
-            input = scanner.nextInt();
+                System.out.println("***********************************************************");
+                System.out.println("To check how much can you buy today fot 100 PLN - enter 1");
+                System.out.println("To compare today with last month currency value - enter 2");
+                System.out.println("Enter - 3 - to end session");
+                input = scanner.nextInt();
 
-            if (input == 1) {
-                count();
-                repeat = true;
-            } else if (input == 2) {
+                if (input == 1) {
+                    count();
+                    repeat = true;
+                } else if (input == 2) {
 
-                compare();
-                repeat = true;
-            } else if (input == 3) repeat = false;
+                    compare();
+                    repeat = true;
+                } else if (input == 3) repeat = false;
+                else throw new InputMismatchException();
+
+            } catch (InputMismatchException inputMismatchException){
+            System.out.printf("%nException: %s%n", inputMismatchException);
+            scanner.nextLine();
+            System.out.printf(
+                    "You must enter integer: 1, 2 or 3. Please try again.%n%n");
+            }
 
         } while (repeat);
 
